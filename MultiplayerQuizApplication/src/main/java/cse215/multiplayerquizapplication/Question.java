b4eb539e81f3c6b0bf3e14bb16b2573b16332c69
BytePaladin <152490925+BytePaladin@users.noreply.github.com>
2024-03-31 21:09:34 +0600

    Add files via upload

---
+++ MultiplayerQuizApplication/src/main/java/cse215/multiplayerquizapplication/Question.java
@@ -0,0 +1,44 @@
+package cse215.multiplayerquizapplication;
+
+import java.util.Scanner;
+
+public class Question {
+    Scanner input = new Scanner(System.in);
+    public int number = 0;
+    private String question, opA, opB, opC, opD;
+    private char ans;
+
+    public Question(String question, String opA, String opB, String opC, String opD, char ans) {
+
+        this.question = question;
+        this.opA = opA;
+        this.opB = opB;
+        this.opC = opC;
+        this.opD = opD;
+        this.ans = ans;
+    }
+
+    public void askQuestion() {
+        System.out.println(question);
+        System.out.println("A. " + opA);
+        System.out.println("B. " + opB);
+        System.out.println("C. " + opC);
+        System.out.println("D. " + opD);
+        System.out.println("Enter your choice : ");
+        char choice = input.next().charAt(0);
+        if (choice == this.ans) {
+            number += 1;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+}
